94b2927d5e1d8038b10db5377f03c2a44437e398
Hemanth <51044943+HemanthR04@users.noreply.github.com>
2023-12-26 13:39:29 +0530

    feat : added Signup Page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,10 +1,28 @@
-import{ ReactNode } from "react";
+import Image from "next/image";
+import { ReactNode } from "react";
+import BackgroundImage from '../../public/login_background.jpg'
+import Logo from '../../public/netflix_logo.svg'
 
-const AuthLayout = ({children} : {children:ReactNode}) => {
+const AuthLayout = ({ children }: { children: ReactNode }) => {
   return (
     <>
-      <div>AuthLayout</div>
-      <div> {children}</div>
+      <div className="relative flex h-screen w-screen bg-black md:items-center md:justify-center md:bg-transparent">
+        <Image src={BackgroundImage}
+        alt="Background Image"
+        className="hidden sm:flex sm:object-cover -z-10 brightness-50"
+        priority
+        fill/>
+
+        <Image
+        src={Logo}
+        alt='Netflix Logo'
+        width={120}
+        height={120}
+        priority
+        className="absolute left-4 top-4 object-contain md:left-10 md:top-10"
+        />
+        {children}
+      </div>
     </>
   );
 };
