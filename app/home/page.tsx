260213955bdcd37a1e0667b4fe1a50956c5a9e52
Hemanth <51044943+HemanthR04@users.noreply.github.com>
2023-12-26 19:41:13 +0530

    feat : Finished Recently added section

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import Navbar from '../components/NavBar'
+import MovieVideo from '../components/MovieVideo'
+import RecentlyAdded from '../components/RecentlyAdded'
 
 const Home = () => {
   return (
-    <Navbar/>
+    <div className='p-5 lg:p-0'>
+       <MovieVideo/>
+       <h1 className='text-3xl font-bold'>Recently Added</h1>
+       <RecentlyAdded/>
+    </div>
+   
   )
 }
 
