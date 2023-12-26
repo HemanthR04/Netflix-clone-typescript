516631191dda1c0600adcc04123e911493ca4846
Hemanth <51044943+HemanthR04@users.noreply.github.com>
2023-12-26 23:08:11 +0530

    fix : server side error

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,17 +1,12 @@
-import React from 'react'
-import Navbar from '../components/NavBar'
-import MovieVideo from '../components/MovieVideo'
-import RecentlyAdded from '../components/RecentlyAdded'
+import MovieVideo from "../components/MovieVideo";
+import RecentlyAdded from "../components/RecentlyAdded";
 
-const Home = () => {
+export default function HomePage() {
   return (
-    <div className='p-5 lg:p-0'>
-       <MovieVideo/>
-       <h1 className='text-3xl font-bold'>Recently Added</h1>
-       <RecentlyAdded/>
+    <div className="p-5 lg:p-0">
+      <MovieVideo />
+      <h1 className="text-3xl font-bold ">Recently Added</h1>
+      <RecentlyAdded />
     </div>
-   
-  )
+  );
 }
-
-export default Home
