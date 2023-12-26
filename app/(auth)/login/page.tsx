b595f07c5437805b5f7ef95192bc9c76a7b87bd3
Hemanth <51044943+HemanthR04@users.noreply.github.com>
2023-12-26 13:43:33 +0530

    feat : added Login Page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,8 +1,36 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import Link from 'next/link'
 import React from 'react'
+import Image from 'next/image'
+import { GithubIcon } from 'lucide-react'
+import GoogleIcon from '../../../public/google.svg'
 
 const Login = () => {
   return (
-    <div>Hello from Login</div>
+    <>
+    <div className='mt-24 rounded bg-black/80 py-10 px-6 md:mt-0 md:max-w-sm md:px-14'>
+        <form action="">
+            <h1 className='text-3xl font-semibold text-white'>Login</h1>
+            <div className='space-y-4 mt-5'>
+                <Input type='email' name='email' placeholder='Email' className='bg-[#333] placeholder:text-xs placeholder:text-gray-400 w-full inline-block'/>
+                <Button type='submit' variant={'destructive'} className='w-full bg-[#e50914] '>Login</Button>
+            </div>
+        </form>
+        <div className='text-gray-500 text-sm mt-2'>
+            New to Netflix?  <Link className='text-white hover:underline' href='/sign-up'>Sign up now!</Link>
+        </div>
+
+        <div className='flex w-full justify-center items-center gap-x-3 mt-6'>
+            <Button variant={'outline'} size='icon'>
+                <GithubIcon className='w-4 h-4'/>
+            </Button>
+            <Button variant={'outline'} size='icon'>
+                <Image src={GoogleIcon} alt='Google Login' className='w-6 h-6'/>
+            </Button>
+        </div>
+    </div>
+    </>
   )
 }
 
