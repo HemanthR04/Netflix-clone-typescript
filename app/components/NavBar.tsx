b0c4b2863c2fdfc3d917d5057b5738a2fe609526
Hemanth <51044943+HemanthR04@users.noreply.github.com>
2023-12-26 16:20:50 +0530

    feat : Navbar updates

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,6 +1,10 @@
+'use client';
 import Image from "next/image";
 import Link from "next/link";
 import Logo from '../../public/netflix_logo.svg'
+import { usePathname } from "next/navigation";
+import { Bell, Search } from "lucide-react";
+import UserNav from "./UserNav";
 
 interface linkProps {
     name:string;
@@ -16,6 +20,7 @@ const links : linkProps[]= [
 ]
 
 export default function Navbar(){
+    const pathName = usePathname()
     return (
         <>
             <div className="w-full max-w-7xl mx-auto items-center justify-between px-5 sm:px-6 py-5 lg:py-8 flex">
@@ -27,7 +32,27 @@ export default function Navbar(){
                         priority
                         />
                     </Link>
-                    <ul className="lg:flex gap-x-4 ml-14 hidden"></ul>
+                    <ul className="lg:flex gap-x-4 ml-14 hidden">
+                        {links.map((link,idx)=>(
+                            <div key={idx}>
+                                {pathName===link.href ? (
+                                    <li>
+                                        <Link href={link.href} className="text-white font-semibold underline">{link.name}</Link>
+                                    </li>
+                                ):(
+                                    <li>
+                                        <Link href={link.href} className="text-gray-300 font-normal">{link.name}</Link>
+                                    </li>
+                                )}
+                            </div>
+                        ))}
+                    </ul>
+                </div>
+
+                <div className="flex items-center gap-x-8">
+                    <Search className="w-5 h-5 text-gray-300 cursor-pointer"/>
+                    <Bell className="w-5 h-5 text-gray-300 cursor-pointer"/>
+                    <UserNav/>
                 </div>
             </div>
         </>
